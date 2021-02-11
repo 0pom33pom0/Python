5c28c637d978497b6369acf623e062d100a7c4a2
Suracha hanthongchi <suracha.h@kkumail.com>
2021-02-11 16:22:17 +0700

    s

---
+++ week1.py
@@ -0,0 +1,17 @@
+"""
+ตัวอย่างที่1
+#name = input("What is your name\n")
+#print("Hi, %s."%name)
+#print("Welcome to Python")
+ตัวอย่างที่2
+#a = 10
+#b = 9
+#if a > b :
+#    print("a > b")
+#    print("ok")
+แบบฝึก1
+name = input("input name : ")
+surname = input("input surname : ")
+year = input("input Year : ")
+code = input("input code : ")
+print(name+"\n"+surname+"\n"+year+"\n"+code)
